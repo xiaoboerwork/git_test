70a58367687b3b979277730dec5219574bfa10fb
xiaoboer <xiaoboer@163.com>
2021-12-24 18:16:31 +0800

    second commit

--- src/test/java/com/xiaoboer/test.java
+++ src/test/java/com/xiaoboer/test.java
@@ -3,5 +3,6 @@ package com.xiaoboer;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world!2");
     }
 }
